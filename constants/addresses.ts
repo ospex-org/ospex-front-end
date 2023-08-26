b38ca8b6e6b3dee9c88f8bdbc21be428a1662701
Vince Laird <vincelaird@gmail.com>
2023-08-26 15:30:52 -0700

    update addresses, abi

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -9,11 +9,11 @@ export const USDCAddress = '0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc'
 export const JsonRpcProviderUrl = 'https://eth-goerli.g.alchemy.com/v2/987gt3TTkj5F4WYbz9mq5ZKytO8BRLNb'
 */
 
-// polygon mumbai w/ functions as of 8/21/2023, solidity version 0.8.18
-export const ContestOracleResolvedAddress = '0x34E3aD450CE15703B19e0BA39504C4C487287864'
-export const CFPv1Address = '0x046dC94519b23c9272712d18Bc00fA73eEd8CF2D'
-export const SpeculationSpreadAddress = '0x8cF06ca8b03950244c6F02d567B61A954C46510F'
-export const SpeculationTotalAddress = '0xEEeC1dc4298F1082554BCBe59b51E790b3967903'
-export const SpeculationMoneylineAddress = '0x324E29e8cEA88FC615F80ba111907b4A67bBa1cD'
+// polygon mumbai w/ functions as of 8/26/2023, solidity version 0.8.18
+export const ContestOracleResolvedAddress = '0x0B556C3650a01Bc779F7afF1f6Bf11166276C9Cb'
+export const CFPv1Address = '0xdfa8538C3eb080959fe357C904798f40753722b2'
+export const SpeculationSpreadAddress = '0x65E155D929eB524BBcA3e0c8982c5fFfC50193b4'
+export const SpeculationTotalAddress = '0x9d1FCC4d4813796cC3239Ea3cEf07444904bfaEA'
+export const SpeculationMoneylineAddress = '0x919C4d11D8bB0c47Ff20DCE7250DE596B78F3E1c'
 export const USDCAddress = '0xd33602ce228adbc90625e4fc8071aae0cad11fe9'
 export const JsonRpcProviderUrl = 'https://polygon-mumbai.g.alchemy.com/v2/8dx7bOITLIXUAIwpMIy_rIGzdlES8hhl'

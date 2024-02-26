50dfe75d84f24d7104b079644699823a30438f88
vincelaird <vincelaird@gmail.com>
2024-02-26 00:38:22 -0500

    suppress duplicate speculations from table

--- sections/table.tsx
+++ sections/table.tsx
@@ -40,25 +40,35 @@ const PrimaryTable = () => {
     router.push('/c/create')
   }
 
-  const filteredSpeculations = speculations
-    ? speculations.filter((speculation) => {
+  const filteredSpeculations = React.useMemo(() => {
+    // Deduplicate speculations based on a combination of contestId and speculationScorer
+    const uniqueSpeculations = new Map()
+    
+    speculations?.forEach((speculation) => {
       const curTime = Date.now() / 1000
       if (speculation.lockTime >= curTime) {
-        if (query === "") {
-          return true
+        const uniqueKey = `${speculation.contestId}-${speculation.speculationScorer}`
+        if (!uniqueSpeculations.has(uniqueKey)) {
+          uniqueSpeculations.set(uniqueKey, speculation)
         }
-        const contestFilterContent = contests.find(
-          (contest) => contest.id === speculation.contestId
-        )
-        return (
-          contestFilterContent?.awayTeam.toLowerCase().includes(query.toLowerCase()) ||
-          contestFilterContent?.homeTeam.toLowerCase().includes(query.toLowerCase()) ||
-          contestFilterContent?.league.toLowerCase().includes(query.toLowerCase())
-        )
       }
-      return false
     })
-    : []
+
+    // Filter based on the search query
+    return Array.from(uniqueSpeculations.values()).filter((speculation) => {
+      if (query === "") {
+        return true
+      }
+      const contestFilterContent = contests.find(
+        (contest) => contest.id === speculation.contestId
+      )
+      return (
+        contestFilterContent?.awayTeam.toLowerCase().includes(query.toLowerCase()) ||
+        contestFilterContent?.homeTeam.toLowerCase().includes(query.toLowerCase()) ||
+        contestFilterContent?.league.toLowerCase().includes(query.toLowerCase())
+      )
+    })
+  }, [speculations, query, contests])
 
   const RenderContent = () => {
     if (isInitialLoad) {

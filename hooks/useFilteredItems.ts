9d90d4c9fb075a5a6c49a4a2cb0b42794e1e3ab6
vincelaird <vincelaird@gmail.com>
2024-03-07 16:49:32 -0500

    remove duplicate speculations from main page

--- hooks/useFilteredItems.ts
+++ hooks/useFilteredItems.ts
@@ -1,17 +1,29 @@
 import { useContext, useMemo } from 'react'
-import { contest } from '../constants/interface'
+import { contest, speculation } from '../constants/interface'
 import { ProviderContext } from '../contexts/ProviderContext'
 
-interface FilterCriteria<T> {
-  (item: T, query: string, contests: contest[]): boolean
+interface DeduplicatedSpeculationCriteria {
+  (item: speculation, index: number, self: speculation[]): boolean
 }
 
-export const useFilteredItems = <T>(query: string, items: T[], filterCriteria: FilterCriteria<T>) => {
+const deduplicateSpeculations: DeduplicatedSpeculationCriteria = (item, index, self) => {
+  return (
+    self.findIndex(
+      (t) =>
+        t.contestId === item.contestId &&
+        t.speculationScorer === item.speculationScorer
+    ) === index
+  )
+}
+
+export const useFilteredItems = <T extends speculation>
+  (query: string, items: T[], filterCriteria: (item: T, query: string, contests: contest[]) => boolean) => {
   const { contests } = useContext(ProviderContext)
+  const deduplicatedItems = useMemo(() => items.filter(deduplicateSpeculations), [items])
 
   const filteredItems = useMemo(() => {
-    return items.filter(item => filterCriteria(item, query, contests))
-  }, [query, items, contests, filterCriteria])
+    return deduplicatedItems.filter(item => filterCriteria(item, query, contests))
+  }, [query, deduplicatedItems, contests, filterCriteria])
 
   return filteredItems
 }
